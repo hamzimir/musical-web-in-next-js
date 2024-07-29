fc9213a520d40df128da88f8e9782306ff6b9d7c
hamzimir <hamzi026psgmail.com>
2024-07-29 11:47:01 -0600

    resolve error in background beam UI file

--- src/components/ui/background-beams.tsx
+++ src/components/ui/background-beams.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {memo} from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/utils/cn";
 export const BackgroundBeams = React.memo(
@@ -135,3 +135,4 @@ export const BackgroundBeams = React.memo(
   }
 );
 
+BackgroundBeams.displayName = "BackgroundBeams";
